b0142932c42fbe51a826a7fcdecbf57ffa20f5e6
rahulvijay81 <118264222+rahulvijay81@users.noreply.github.com>
2024-04-16 13:58:38 +0530

    emoji picker

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,22 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Emoji Picker",
-  description: "Easily Express Yourself Online! Discover a vast of emojis to add flair, humor, or emotion to your messages. Whether chatting, emailing, or posting on social media, find the perfect emoji to convey your thoughts instantly.",
+  description:
+    "Easily Express Yourself Online! Discover a vast of emojis to add flair, humor, or emotion to your messages. Whether chatting, emailing, or posting on social media, find the perfect emoji to convey your thoughts instantly.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
